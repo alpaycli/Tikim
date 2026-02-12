bf557890712c8a2b10692b72c066ae62363454b6
Alpay Calalli <calalli1903@gmail.com>
2026-02-12 11:35:36 +0400

    Initial Commit

---
+++ Tikim/TikimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikimApp.swift
+//  Tikim
+//
+//  Created by Alpay Calalli on 12.02.26.
+//
+
+import SwiftUI
+
+@main
+struct TikimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
